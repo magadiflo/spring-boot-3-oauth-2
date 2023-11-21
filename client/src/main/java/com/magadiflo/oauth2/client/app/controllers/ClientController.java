71debb334703c0d9553888dc34fd6b98176c5729
Martín <magadiflo@gmail.com>
2023-11-21 13:14:24 -0500

    Endpoint para llamar al servidor de recurso

--- client/src/main/java/com/magadiflo/oauth2/client/app/controllers/ClientController.java
+++ client/src/main/java/com/magadiflo/oauth2/client/app/controllers/ClientController.java
@@ -6,10 +6,18 @@ import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2Aut
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class ClientController {
+
+    private final WebClient webClient;
+
+    public ClientController(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("http://127.0.0.1:9090").build();
+    }
+
     @GetMapping(path = "/home")
     public Mono<String> home(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client, @AuthenticationPrincipal OidcUser oidcUser) {
         return Mono.just("""
@@ -22,5 +30,14 @@ public class ClientController {
                 client.getRefreshToken().getTokenValue(),       // Pertenece al cliente
                 oidcUser.getIdToken().getTokenValue(),          // Pertenece al usuario logueado
                 oidcUser.getClaims()));                         // Pertenece al usuario logueado
+    }
+
+    @GetMapping(path = "/tasks")
+    public Mono<String> getTasks(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client) {
+        return this.webClient.get()
+                .uri("tasks")
+                .header("Authorization", "Bearer %s".formatted(client.getAccessToken().getTokenValue()))
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
